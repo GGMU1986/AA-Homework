f1a3848017ca7ec66e7f00e8904797158439ab99
gtsimis <gt2186@gmail.com>
2021-08-31 07:52:28 -0400

    Added initialize and valid_moves to Board Class(Mancala)

--- W4D2/skeleton/lib/board.rb
+++ W4D2/skeleton/lib/board.rb
@@ -1,14 +1,27 @@
+require_relative "player"
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { Array.new }
+    @player1 = Player.new(name1, @cups[0..5])
+    @player2 = Player.new(name2, @cups[7..12])
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      if i != 6 && i != 13
+        4.times { cup << :stone }
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    spots = (0..13).to_a
+    raise "Invalid starting cup" unless spots.include?(start_pos)
+    raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)

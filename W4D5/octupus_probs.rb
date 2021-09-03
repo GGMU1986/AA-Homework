eb303203986d9722f5e898103291e1ec63fd6202
gtsimis <gt2186@gmail.com>
2021-09-03 08:41:25 -0400

    Added first part of dominant octopus

--- W4D5/octupus_probs.rb
+++ W4D5/octupus_probs.rb
@@ -16,9 +16,34 @@ def sluggish_octopus(arr)
     longest
 end
 
+def dominant_octopus(arr)
+    return arr if arr.length <= 1
+    mid = arr.length / 2
+
+    left_sorted = dominant_octopus(arr.take(mid))
+    right_sorted = dominant_octopus(arr.drop(mid))
+
+    merge(left_sorted, right_sorted)
+    arr.last
+end
+
+def merge(left, right)
+    merged = []
+
+    until left.empty? || right.empty?
+        if left.first.length < right.first.length
+            merged << left.shift
+        else
+            merged << right.shift
+        end
+    end
+    merged + left + right
+end
+
 def clever_octopus(arr)
     arr.inject { |acc, el| acc.length > el.length ? acc : el }
 end
 
 # p sluggish_octopus(FISH)
-p clever_octopus(FISH)
+# p clever_octopus(FISH)
+p dominant_octopus(FISH)

d32c59d605edf2134ce3cd406137fa25b603a8ba
gtsimis <gt2186@gmail.com>
2021-09-22 08:12:27 -0400

    Added validations and tests

--- W7D3/Capybara_HW/CapybaraHW/app/models/user.rb
+++ W7D3/Capybara_HW/CapybaraHW/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     validates :email, :password_digest, presence: true
     validates :password, length: { minimum: 6 }, allow_nil: true
+
+    def password
+        @password
+    end
 end
--- W7D3/Capybara_HW/CapybaraHW/config/environments/test.rb
+++ W7D3/Capybara_HW/CapybaraHW/config/environments/test.rb
@@ -40,8 +40,10 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-
+  config.active_record.migration_error = true
+  # config.active_record.migration_error = false 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   Rails.application.routes.default_url_options[:host] = 'domain.com'
+  
 end

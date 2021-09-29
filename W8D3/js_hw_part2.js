6792926f868d2413da283e1d1045ad33c65aa148
gtsimis <gt2186@gmail.com>
2021-09-29 09:00:24 -0400

    Added grow and trumpet methods

--- W8D3/js_hw_part2.js
+++ W8D3/js_hw_part2.js
@@ -1,12 +1,40 @@
-let callback = function(names) {
-    names.forEach(name => {
-        console.log(name);
-    }) 
+// let callback = function(names) {
+//     names.forEach(name => {
+//         console.log(name);
+//     }) 
+// };
+
+// function titleize(names, callback) {
+//     callback(names.map(name => `Mx. ${name} Jingleheimer Schmidt`)); 
+// };
+
+// titleize(["Mary", "Brian", "Leo"], callback);
+
+function Elephant(name, height, tricks) {
+    this.name = name
+    this.height = height
+    this.tricks = tricks
 };
 
-function titleize(names, callback) {
-    callback(names.map(name => `Mx. ${name} Jingleheimer Schmidt`)); 
+let joey = new Elephant('joey', 72, ['playing the guitar', 'eating all the food']);
+
+Elephant.prototype.trumpet = function() {
+    console.log(`${this.name} the elephant goes phrRRRRRRRRRRR!!!!!!!`);
 };
 
-titleize(["Mary", "Brian", "Leo"], callback);
+Elephant.prototype.grow = function () {
+    this.height += 12;
+};
 
+// Elephant.prototype.addTrick(trick) = function() {
+//     this.tricks.push(trick)
+// }
+
+// Elephant.prototype.play = function() {
+//     randomTrick = this.tricks[Math.floor(Math.random() * this.tricks.length)];
+//     console.log(randomTrick);
+// }
+
+console.log(joey.height)
+joey.grow()
+console.log(joey.height)

286d6ab09978fc848b216336f53b29869a2dee52
gtsimis <gt2186@gmail.com>
2021-09-29 08:36:17 -0400

    Finished Titleize

--- W8D3/js_hw_part2.js
+++ W8D3/js_hw_part2.js
@@ -1,12 +1,12 @@
-function titleize(names, callback) {
-    Array.prototype.map(name => {
-        callback(`Mx. ${name} Jingleheimer Schmidt`);
-    })
-};
-
 let callback = function(names) {
     names.forEach(name => {
-        console.log(name)
-      }) 
-    };
+        console.log(name);
+    }) 
+};
+
+function titleize(names, callback) {
+    callback(names.map(name => `Mx. ${name} Jingleheimer Schmidt`)); 
+};
+
+titleize(["Mary", "Brian", "Leo"], callback);
 

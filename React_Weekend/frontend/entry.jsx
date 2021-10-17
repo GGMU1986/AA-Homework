2055ec27fdedad99ab2b8ed74b128d0e5845ad52
gtsimis <gt2186@gmail.com>
2021-10-17 11:33:15 -0400

    Added Header/Footer compnents to root App component

--- React_Weekend/frontend/entry.jsx
+++ React_Weekend/frontend/entry.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MyInfo from "./MyInfo"
+import App from "./App"
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<MyInfo />, document.getElementById('root'))
+    ReactDOM.render(<App />, document.getElementById('root'))
 })

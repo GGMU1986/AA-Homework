af637be21a9f5e13b41c895a76a7a29d816cc576
gtsimis <gt2186@gmail.com>
2021-10-16 20:13:24 -0400

    Added Tabs component

--- widgets_redo/frontend/widgets.jsx
+++ widgets_redo/frontend/widgets.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import Clock from './clock'
 import Weather from './weather'
+import Tabs from './tabs'
 
-
+const panes = [
+    { title: "one", content: "I am one" },
+    { title: "two", content: "I am two" },
+    { title: "three", content: "I am three" }  
+]
 
 function Root() {
     return (
         <div>
             <Clock />
             <Weather />
+            <Tabs />
         </div>
     )
 };

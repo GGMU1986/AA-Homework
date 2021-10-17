9e7f02cc768a50b09d7d99cc0f97cd71d0cdea0b
gtsimis <gt2186@gmail.com>
2021-10-17 10:36:27 -0400

    Made function component MyApp, React still works

--- React_Weekend/entry.jsx
+++ React_Weekend/entry.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root')
-    ReactDOM.render(
+function MyApp() {
+    return (
         <ul>
             <li>1</li>
             <li>2</li>
             <li>3</li>
-        </ul> , 
-        root
+        </ul>
     )
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root')
+    ReactDOM.render(<MyApp />, root)
 })

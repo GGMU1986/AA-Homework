35997268c9faa75349d14c87498d5077df047e28
gtsimis <gt2186@gmail.com>
2021-10-17 10:32:03 -0400

    Set up webpack and rendered on broswer thru React

---
+++ React_Weekend/entry.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root')
+    ReactDOM.render(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul> , 
+        root
+    )
+})

40d07c908a19fc20b6bf137f3de86e8bfb6151ad
gtsimis <gt2186@gmail.com>
2021-10-16 20:03:23 -0400

    Added weather component, still needs work

--- widgets_redo/frontend/weather.jsx
+++ widgets_redo/frontend/weather.jsx
@@ -1,7 +1,20 @@
-// import React from "react";
+import React from "react";
 
-// class Weather extends React.Component {
-    
-// }
+class Weather extends React.Component {
+    constructor(props) {
+        super(props)
+    }
 
-// export default Weather;
+    render() {
+        return (
+            <div>
+                <h1>Weather</h1>
+                <div class="weather">
+                    <p>loading weather ...</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Weather;
--- widgets_redo/frontend/widgets.jsx
+++ widgets_redo/frontend/widgets.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import Clock from './clock'
-//import Weather from './weather'
+import Weather from './weather'
+
+
 
 function Root() {
     return (
         <div>
             <Clock />
-            {/* <Weather /> */}
+            <Weather />
         </div>
     )
 };
